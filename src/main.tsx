2342fd0d8868c1b612db09cd0cb07a0e0f5ba2be
zolldic <ojiip95@gmail.com>
2026-04-02 13:29:33 +0300

    chore(react): enable StrictMode for development

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 
   import { createRoot } from "react-dom/client";
+  import { StrictMode } from "react";
   import App from "./App.tsx";
   import "./index.css";
 
-  createRoot(document.getElementById("root")!).render(<App />);
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
   
