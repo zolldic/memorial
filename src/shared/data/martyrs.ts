21fa9b40a8741775294b8110ab6edc269ae907a4
zolldic <ojiip95@gmail.com>
2026-04-05 19:27:15 +0300

    refactor(martyrs): restructure martyr data format for improved clarity and consistency

--- src/shared/data/martyrs.ts
+++ src/shared/data/martyrs.ts
@@ -7,7 +7,6 @@ export const martyrsData: Martyr[] = [
   dateOfMartyrdom: "2022-07-21",
   image: "/1.jpeg",
   candles: 0,
-
   name: {
     en: "Abubakr Moatasem Ismail",
     ar: "أبوبكر معتصم إسماعيل"
@@ -24,16 +23,120 @@ export const martyrsData: Martyr[] = [
     en: "Student",
     ar: "طالب"
   },
-
-  nameEn: "Abubakr Moatasem Ismail",
-  nameAr:  "أبوبكر معتصم إسماعيل",
-
-  locationEn: "Omdurman",
-  locationAr: "أم درمان",
-  storyEn: "On July 21, 2022, during a demonstration at Az-Zuhri Square, Abubakr Moatasem Ismail was shot in the chest and martyred at the age of 22. Known among friends as 'Ghaimah,' he was more than a participant; he was a light spirit amid hardship, always smiling and uplifting others. He carried simple yet profound dreams: to impact his country's economy, even by 1%. Brave in the face of danger, he protected others and remained present in both struggles and small moments, offering laughter, support, and humanity.",
-  storyAr: "في يوم 21 يوليو 2022، وأثناء موكب في صينية الأزهري، أُصيب أبوبكر معتصم إسماعيل برصاصة في الصدر، ليرتقي شهيدًا وهو في الثانية والعشرين من عمره. عرفه أصدقاؤه باسم 'غيمة'، وكان أكثر من مجرد مشارك؛ كان روحًا خفيفة وسط القسوة، مبتسمًا دائمًا ويرفع من معنويات من حوله. كان يحمل أحلامًا بسيطة لكنها عميقة: أن يؤثر في اقتصاد بلده بنسبة 1%. شجاعًا في مواجهة الخطر، كان يحمي الآخرين، ويشارك في الصعاب والتفاصيل الصغيرة، مقدمًا الضحكة والدعم والإنسانية.",
-  professionEn: "Student",
-  professionAr: "طالب",
-}
-
+},
+{
+  id: "2",
+  age: 22,
+  dateOfMartyrdom: "2022-07-21",
+  image: "/1.jpeg",
+  candles: 0,
+  name: {
+    en: "Abubakr Moatasem Ismail",
+    ar: "أبوبكر معتصم إسماعيل"
+  },
+  location: {
+    en: "Omdurman",
+    ar: "أم درمان"
+  },
+  story: {
+    en: "On July 21, 2022, during a demonstration at Az-Zuhri Square, Abubakr Moatasem Ismail was shot in the chest and martyred at the age of 22. Known among friends as 'Ghaimah,' he was more than a participant; he was a light spirit amid hardship, always smiling and uplifting others. He carried simple yet profound dreams: to impact his country's economy, even by 1%. Brave in the face of danger, he protected others and remained present in both struggles and small moments, offering laughter, support, and humanity.",
+    ar: "في يوم 21 يوليو 2022، وأثناء موكب في صينية الأزهري، أُصيب أبوبكر معتصم إسماعيل برصاصة في الصدر، ليرتقي شهيدًا وهو في الثانية والعشرين من عمره. عرفه أصدقاؤه باسم 'غيمة'، وكان أكثر من مجرد مشارك؛ كان روحًا خفيفة وسط القسوة، مبتسمًا دائمًا ويرفع من معنويات من حوله. كان يحمل أحلامًا بسيطة لكنها عميقة: أن يؤثر في اقتصاد بلده بنسبة 1%. شجاعًا في مواجهة الخطر، كان يحمي الآخرين، ويشارك في الصعاب والتفاصيل الصغيرة، مقدمًا الضحكة والدعم والإنسانية."
+  },
+  profession: {
+    en: "Student",
+    ar: "طالب"
+  },
+},
+{
+  id: "3",
+  age: 22,
+  dateOfMartyrdom: "2022-07-21",
+  image: "/1.jpeg",
+  candles: 0,
+  name: {
+    en: "Abubakr Moatasem Ismail",
+    ar: "أبوبكر معتصم إسماعيل"
+  },
+  location: {
+    en: "Omdurman",
+    ar: "أم درمان"
+  },
+  story: {
+    en: "On July 21, 2022, during a demonstration at Az-Zuhri Square, Abubakr Moatasem Ismail was shot in the chest and martyred at the age of 22. Known among friends as 'Ghaimah,' he was more than a participant; he was a light spirit amid hardship, always smiling and uplifting others. He carried simple yet profound dreams: to impact his country's economy, even by 1%. Brave in the face of danger, he protected others and remained present in both struggles and small moments, offering laughter, support, and humanity.",
+    ar: "في يوم 21 يوليو 2022، وأثناء موكب في صينية الأزهري، أُصيب أبوبكر معتصم إسماعيل برصاصة في الصدر، ليرتقي شهيدًا وهو في الثانية والعشرين من عمره. عرفه أصدقاؤه باسم 'غيمة'، وكان أكثر من مجرد مشارك؛ كان روحًا خفيفة وسط القسوة، مبتسمًا دائمًا ويرفع من معنويات من حوله. كان يحمل أحلامًا بسيطة لكنها عميقة: أن يؤثر في اقتصاد بلده بنسبة 1%. شجاعًا في مواجهة الخطر، كان يحمي الآخرين، ويشارك في الصعاب والتفاصيل الصغيرة، مقدمًا الضحكة والدعم والإنسانية."
+  },
+  profession: {
+    en: "Student",
+    ar: "طالب"
+  },
+},
+{
+  id: "4",
+  age: 22,
+  dateOfMartyrdom: "2022-07-21",
+  image: "/1.jpeg",
+  candles: 0,
+  name: {
+    en: "Abubakr Moatasem Ismail",
+    ar: "أبوبكر معتصم إسماعيل"
+  },
+  location: {
+    en: "Omdurman",
+    ar: "أم درمان"
+  },
+  story: {
+    en: "On July 21, 2022, during a demonstration at Az-Zuhri Square, Abubakr Moatasem Ismail was shot in the chest and martyred at the age of 22. Known among friends as 'Ghaimah,' he was more than a participant; he was a light spirit amid hardship, always smiling and uplifting others. He carried simple yet profound dreams: to impact his country's economy, even by 1%. Brave in the face of danger, he protected others and remained present in both struggles and small moments, offering laughter, support, and humanity.",
+    ar: "في يوم 21 يوليو 2022، وأثناء موكب في صينية الأزهري، أُصيب أبوبكر معتصم إسماعيل برصاصة في الصدر، ليرتقي شهيدًا وهو في الثانية والعشرين من عمره. عرفه أصدقاؤه باسم 'غيمة'، وكان أكثر من مجرد مشارك؛ كان روحًا خفيفة وسط القسوة، مبتسمًا دائمًا ويرفع من معنويات من حوله. كان يحمل أحلامًا بسيطة لكنها عميقة: أن يؤثر في اقتصاد بلده بنسبة 1%. شجاعًا في مواجهة الخطر، كان يحمي الآخرين، ويشارك في الصعاب والتفاصيل الصغيرة، مقدمًا الضحكة والدعم والإنسانية."
+  },
+  profession: {
+    en: "Student",
+    ar: "طالب"
+  },
+},
+{
+  id: "5",
+  age: 22,
+  dateOfMartyrdom: "2022-07-21",
+  image: "/1.jpeg",
+  candles: 0,
+  name: {
+    en: "Abubakr Moatasem Ismail",
+    ar: "أبوبكر معتصم إسماعيل"
+  },
+  location: {
+    en: "Omdurman",
+    ar: "أم درمان"
+  },
+  story: {
+    en: "On July 21, 2022, during a demonstration at Az-Zuhri Square, Abubakr Moatasem Ismail was shot in the chest and martyred at the age of 22. Known among friends as 'Ghaimah,' he was more than a participant; he was a light spirit amid hardship, always smiling and uplifting others. He carried simple yet profound dreams: to impact his country's economy, even by 1%. Brave in the face of danger, he protected others and remained present in both struggles and small moments, offering laughter, support, and humanity.",
+    ar: "في يوم 21 يوليو 2022، وأثناء موكب في صينية الأزهري، أُصيب أبوبكر معتصم إسماعيل برصاصة في الصدر، ليرتقي شهيدًا وهو في الثانية والعشرين من عمره. عرفه أصدقاؤه باسم 'غيمة'، وكان أكثر من مجرد مشارك؛ كان روحًا خفيفة وسط القسوة، مبتسمًا دائمًا ويرفع من معنويات من حوله. كان يحمل أحلامًا بسيطة لكنها عميقة: أن يؤثر في اقتصاد بلده بنسبة 1%. شجاعًا في مواجهة الخطر، كان يحمي الآخرين، ويشارك في الصعاب والتفاصيل الصغيرة، مقدمًا الضحكة والدعم والإنسانية."
+  },
+  profession: {
+    en: "Student",
+    ar: "طالب"
+  },
+},
+{
+  id: "6",
+  age: 22,
+  dateOfMartyrdom: "2022-07-21",
+  image: "/1.jpeg",
+  candles: 0,
+  name: {
+    en: "Abubakr Moatasem Ismail",
+    ar: "أبوبكر معتصم إسماعيل"
+  },
+  location: {
+    en: "Omdurman",
+    ar: "أم درمان"
+  },
+  story: {
+    en: "On July 21, 2022, during a demonstration at Az-Zuhri Square, Abubakr Moatasem Ismail was shot in the chest and martyred at the age of 22. Known among friends as 'Ghaimah,' he was more than a participant; he was a light spirit amid hardship, always smiling and uplifting others. He carried simple yet profound dreams: to impact his country's economy, even by 1%. Brave in the face of danger, he protected others and remained present in both struggles and small moments, offering laughter, support, and humanity.",
+    ar: "في يوم 21 يوليو 2022، وأثناء موكب في صينية الأزهري، أُصيب أبوبكر معتصم إسماعيل برصاصة في الصدر، ليرتقي شهيدًا وهو في الثانية والعشرين من عمره. عرفه أصدقاؤه باسم 'غيمة'، وكان أكثر من مجرد مشارك؛ كان روحًا خفيفة وسط القسوة، مبتسمًا دائمًا ويرفع من معنويات من حوله. كان يحمل أحلامًا بسيطة لكنها عميقة: أن يؤثر في اقتصاد بلده بنسبة 1%. شجاعًا في مواجهة الخطر، كان يحمي الآخرين، ويشارك في الصعاب والتفاصيل الصغيرة، مقدمًا الضحكة والدعم والإنسانية."
+  },
+  profession: {
+    en: "Student",
+    ar: "طالب"
+  },
+},
 ];
